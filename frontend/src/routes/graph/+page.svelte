41a86ad868153b1a18615fd5508298404034772b
pablofm <pablofm@us.es>
2023-04-14 11:33:48 +0200

    graph init

---
+++ frontend/src/routes/graph/+page.svelte
@@ -0,0 +1,94 @@
+<svelte:head>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/series-label.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+</svelte:head>
+
+<script>
+    // @ts-nocheck
+    import {onMount} from 'svelte';
+
+    async function loadChart(){
+
+        Highcharts.chart('container', {
+
+            title: {
+                text: 'U.S Solar Employment Growth by Job Category, 2010-2020',
+                align: 'left'
+            },
+
+            subtitle: {
+                text: 'Source: <a href="https://irecusa.org/programs/solar-jobs-census/" target="_blank">IREC</a>',
+                align: 'left'
+            },
+
+            yAxis: {
+                title: {
+                    text: 'Number of Employees'
+                }
+            },
+
+            xAxis: {
+                accessibility: {
+                    rangeDescription: 'Range: 2010 to 2020'
+                }
+            },
+
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle'
+            },
+
+            plotOptions: {
+                series: {
+                    label: {
+                        connectorAllowed: false
+                    },
+                    pointStart: 2010
+                }
+            },
+
+            series: [{
+                name: 'Installation & Developers',
+                data: [43934, 48656, 65165, 81827, 112143, 142383,
+                    171533, 165174, 155157, 161454, 154610]
+            }],
+
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        legend: {
+                            layout: 'horizontal',
+                            align: 'center',
+                            verticalAlign: 'bottom'
+                        }
+                    }
+                }]
+            }
+
+            });
+
+    }
+
+    onMount(async () =>{
+        loadChart();
+    });
+
+
+</script>
+
+<main>
+    <h1>Graph</h1>
+    <figure class="highcharts-figure">
+        <div id="container"></div>
+        <p class="highcharts-description">
+    
+        </p>
+    </figure>    
+</main>
